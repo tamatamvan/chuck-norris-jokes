48afe9f6bf620fd7e19e166c06489673b3cce03c
Septian A Tama <sfirst.tama@gmail.com>
2018-03-11 01:07:03 +0700

    fetching data from api

--- components/Joke.js
+++ components/Joke.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native'
+import axios from 'axios'
 
 class componentName extends Component {
+  constructor () {
+    super()
+    this.state = {
+      loading: false,
+      error: false,
+      joke: {
+        icon_url: '',
+        id: '',
+        url: '',
+        value: '',
+      },
+    }
+  }
+
+  componentWillMount () {
+    axios.get('https://api.chucknorris.io/jokes/random')
+      .then(({data}) => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <View>
