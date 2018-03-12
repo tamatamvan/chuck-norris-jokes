799ee38198df13ec89a22db8f42213b2cfafe88d
Septian A Tama <sfirst.tama@gmail.com>
2018-03-12 22:41:28 +0700

    add about screen

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Joke from './components/Joke';
+import About from './components/About';
 
 const RootStack = StackNavigator({
   Home: {
     screen: Joke
-  }
+  },
+  About: {
+    screen: About
+  },
 }, {
   initialRouteName: 'Home'
 })
