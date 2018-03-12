0ddd2a24f39b310cdbd070c318c093e177eb36bc
Septian A Tama <sfirst.tama@gmail.com>
2018-03-12 22:36:59 +0700

    initiating stack navigator

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 import Joke from './components/Joke';
+
+const RootStack = StackNavigator({
+  Home: {
+    screen: Joke
+  }
+}, {
+  initialRouteName: 'Home'
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Joke/>
-      </View>
+      <RootStack/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    display: 'flex',
-  },
-});
